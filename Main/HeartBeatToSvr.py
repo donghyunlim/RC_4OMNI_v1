16e02e4e3ef335eb9e5ceb71486f14be8adb5090
donny <ipsak2.dl@gmail.com>
2021-03-14 06:59:06 +0000

    Add http request infinite retry code when exception happend

--- Main/HeartBeatToSvr.py
+++ Main/HeartBeatToSvr.py
@@ -2,6 +2,7 @@
 import threading
 import requests
 import RegistrationToSvr
+from time import sleep
 
 SVR_BASE_ADDR = 'http://onffworld.iptime.org:48080' #todo: https 보안 스트림으로의 업그레이드
 RESTFUL_EXPRESSION = ''
@@ -23,22 +24,26 @@ class HeartBeating():
         mediasvrPort = _mediasvrPort
             
     def heartbeating(self):
-        
-        infos = {'type_of_rc': rctype, 'serial_of_rc': RegistrationToSvr.getserial(), 'ipv4_private': privIP, 'ipv4_public':publicIP
-          , 'wanted_ddns_websvr_port':websvrPort, 'wanted_ddns_mediasvr_port':mediasvrPort}
-        response = requests.post(SVR_BASE_ADDR+RESTFUL_EXPRESSION, params=infos) #can be params, or simply json.
-        # response = requests.post(SVR_BASE_ADDR+RESTFUL_EXPRESSION, data=infos)
         try:
-            response.raise_for_status()
-            if(response.status_code == 200 or response.status_code == 201): #good
-                pass
-                # print('all good!')
-            else: #not good, but received anyway.
-                pass
-                # print('server alive but went wrong. maybe there are some mistaken things?')
-            
-        except requests.exceptions.HTTPError as e:
-            print('bam! error occured while connecting to the static server')
+            infos = {'type_of_rc': rctype, 'serial_of_rc': RegistrationToSvr.getserial(), 'ipv4_private': privIP, 'ipv4_public':publicIP
+            , 'wanted_ddns_websvr_port':websvrPort, 'wanted_ddns_mediasvr_port':mediasvrPort}
+            response = requests.post(SVR_BASE_ADDR+RESTFUL_EXPRESSION, params=infos) #can be params, or simply json.
+            # response = requests.post(SVR_BASE_ADDR+RESTFUL_EXPRESSION, data=infos)
+            try:
+                response.raise_for_status()
+                if(response.status_code == 200 or response.status_code == 201): #good
+                    pass
+                    # print('all good!')
+                else: #not good, but received anyway.
+                    pass
+                    # print('server alive but went wrong. maybe there are some mistaken things?')
+                
+            except requests.exceptions.HTTPError as e:
+                print('bam! error occured while connecting to the static server')
 
-        print("Send my alive signal to server ==>  "+SVR_BASE_ADDR+RESTFUL_EXPRESSION)
-        threading.Timer(55, self.heartbeating).start() #every 55(60)seconds (we need latency*2+5% more rapidly)
+            print("Send my alive signal to server ==>  "+SVR_BASE_ADDR+RESTFUL_EXPRESSION)
+            threading.Timer(55, self.heartbeating).start() #every 55(60)seconds (we need latency*2+5% more rapidly)
+        except:
+            print('Heartbeater : Cannot connect to our server(or web), retry after 5 secs')
+            sleep(5) #5 sec to retry
+            self.heartbeating()#do nothing, just retry.
--- Main/RegistrationToSvr.py
+++ Main/RegistrationToSvr.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import socket
+from time import sleep
 
 def getserial():
   # Extract serial from cpuinfo file
@@ -20,35 +21,47 @@ def getserial():
 SVR_BASE_ADDR = 'http://onffworld.iptime.org:33774' #todo: https 보안 스트림으로의 업그레이드
 RESTFUL_EXPRESSION = '/rc'
 # response = requests.post(SVR_BASE_ADDR+RESTFUL_EXPRESSION)
-
 typeOfRc = 'Rc4WheelV1'
-try: #could be exception because this is external address.
-  ipv4Public = format(requests.get('https://api.ipify.org').text)
-except:
-  ipv4Public = format(requests.get('https://api.ipify.org').text) #anyway, do one more time if exception raise
-# get ip address by google. we'll have to check it always return private addr.
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-ipv4Private = s.getsockname()[0]
-s.close()
-# ipv4Private = socket.gethostbyname(socket.gethostname()) #if /etc/hosts describes lo, then always return 127.0.0.1
-ipaddrLastThree = ipv4Private.split('.')
-wantedDdnsMediasvrPort = 35000+int(ipaddrLastThree[3])
-wantedDdnsWebsvrPort = 25000+int(ipaddrLastThree[3])
 
-infos = {'type_of_rc': typeOfRc, 'serial_of_rc': getserial(), 'ipv4_private': ipv4Private, 'ipv4_public':ipv4Public
-          , 'wanted_ddns_websvr_port':wantedDdnsWebsvrPort, 'wanted_ddns_mediasvr_port':wantedDdnsMediasvrPort}
-response = requests.put(SVR_BASE_ADDR+RESTFUL_EXPRESSION, data=infos)
+def setInfoToWebsvr():
+  global ipv4Public
+  global ipv4Private
+  global wantedDdnsMediasvrPort
+  global wantedDdnsWebsvrPort
+  try:
+    try: #could be exception because this is external address.
+      ipv4Public = format(requests.get('https://api.ipify.org').text)
+    except:
+      ipv4Public = format(requests.get('https://api.ipify.org').text) #anyway, do one more time if exception raise
+    # get ip address by google. we'll have to check it always return private addr.
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ipv4Private = s.getsockname()[0]
+    s.close()
+    # ipv4Private = socket.gethostbyname(socket.gethostname()) #if /etc/hosts describes lo, then always return 127.0.0.1
+    ipaddrLastThree = ipv4Private.split('.')
+    wantedDdnsMediasvrPort = 35000+int(ipaddrLastThree[3])
+    wantedDdnsWebsvrPort = 25000+int(ipaddrLastThree[3])
 
-try:
-    response.raise_for_status()
-    if(response.status_code == 200 or response.status_code == 201): #정상
-        print('all good! regi success')
-    else: #비정상, 반영실패 응답. 특히 202
-        print('server alive but went wrong. maybe there are some mistaken things?')
-    
-except requests.exceptions.HTTPError as e:
-    print('bam! error occured while connecting to the static server')
+    infos = {'type_of_rc': typeOfRc, 'serial_of_rc': getserial(), 'ipv4_private': ipv4Private, 'ipv4_public':ipv4Public
+              , 'wanted_ddns_websvr_port':wantedDdnsWebsvrPort, 'wanted_ddns_mediasvr_port':wantedDdnsMediasvrPort}
+    response = requests.put(SVR_BASE_ADDR+RESTFUL_EXPRESSION, data=infos)
+
+    try:
+        response.raise_for_status()
+        if(response.status_code == 200 or response.status_code == 201): #정상
+            print('all good! regi success')
+        else: #비정상, 반영실패 응답. 특히 202
+            print('server alive but went wrong. maybe there are some mistaken things?')
+        
+    except requests.exceptions.HTTPError as e:
+        print('bam! error occured while connecting to the static server')
+  except:
+    print('Cannot connect to our server(or web), retry after 5 secs')
+    sleep(5) #5 sec to retry
+    setInfoToWebsvr()#do nothing, just retry.
+
+setInfoToWebsvr()
 
 #interface for another code.
 class Getter():
